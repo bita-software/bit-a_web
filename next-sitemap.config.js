5a57bbb7879d4aa45e1c5270bf8680f6ffc683d5
Ignacio Gomez <igomez@arcanus.com.uy>
2025-07-28 08:12:47 -0300

    Add sitemap and robots.txt configuration
    - Introduced `next-sitemap` for automatic sitemap generation.
    - Created `next-sitemap.config.js` to configure sitemap settings.
    - Added `robots.txt` to allow search engine indexing.
    - Generated initial `sitemap.xml` structure.

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://bit-a.com',
+    generateRobotsTxt: true,
+    changefreq: 'monthly',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
